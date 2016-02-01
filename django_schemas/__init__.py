22ad951ab78c578051f4a7fd8b88323af93c25f7
Ryan N Johnson <ryan@discoveryhill.co>
2016-01-31 16:52:37 -0800

    getting setup going

---
+++ django_schemas/__init__.py
@@ -0,0 +1,7 @@
+"""
+Django Schemas is an extension for Python's Django framework. It
+supports multiple schemas and migrations for PostgreSQL.
+"""
+
+__license__ = 'GNU General Public License v3.0'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,3 +1,31 @@
-from setuptools import setup
+import os.path
+import re
+from setuptools import find_packages, setup
 
 
+def get_info(var):
+    """Get version from the package."""
+    with open(os.path.join('django_schemas','__init__.py')) as f:
+        content = f.read()
+    return re.search(var + r'\s*=\s*["\'](.+?)["\']', content).group(1)
+
+
+VERSION = get_info('__version__')
+LICENSE = get_info('__license__')
+
+
+setup(
+        name="django_schemas",
+        description="Postgres schemas for Django.",
+        url="https://github.com/ryannjohnson/django-schemas",
+        license=LICENSE,
+        version=VERSION,
+        packages=find_packages('django_schemas'),
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Framework :: Django :: 1.8',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3.4',
+        ])
